d70eed01f3fea96e014b4fff35c861269c84c455
Matheus Viersa <matheusviersa995@hotmail.com>
2025-05-05 11:59:53 -0300

    Primeiro commit

---
+++ src/pck/Tabela.java
@@ -0,0 +1,19 @@
+package pck;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.Target;
+
+
+
+
+
+
+
+@Retention(RetentionPolicy.RUNTIME);
+@Target(ElementType.TYPE);
+public @interface Tabela{
+	String nome();
+	String valor();
+}
